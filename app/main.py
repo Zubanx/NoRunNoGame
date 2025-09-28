a0822a1d5d6bcd720eab43e9d11f225bd69a3df6
Javier Chavez <cjavier0315@gmail.com>
2025-09-28 14:50:08 -0400

    Added get user functionality

--- app/main.py
+++ app/main.py
@@ -6,10 +6,22 @@ from fastapi.responses import RedirectResponse
 import os
 from dotenv import load_dotenv
 import requests
+from pydantic import BaseModel 
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 
+access_token =""
+
+class User(BaseModel):
+    first_name : str
+    last_name : str
+    city : str
+
+    
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 load_dotenv()
 
@@ -18,6 +30,9 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+@app.get("/")
+async def read_root():
+    return FileResponse("static/login.html")
 
 @app.get("/login")
 async def login():
@@ -43,8 +58,24 @@ async def dashboard(code : str):
         "grant_type": "authorization_code"
     }
     url = "https://www.strava.com/oauth/token"
-    token_response = requests.post(url, params=params)
+    token_response = requests.post(url, data=params)
     if token_response.status_code == 200:
         token_data = token_response.json()
         access_token = token_data["access_token"]
-    return RedirectResponse("http://127.0.0.1:5500/dashboard.html")
+        print(f"Expires in: {token_data["expires_in"]}")
+        print(access_token)
+    return RedirectResponse("/static/dashboard.html")
+
+@app.get("/user")
+async def get_user():
+    url = "https://www.strava.com/api/v3/athlete"
+    headers = {f"Authorization" : "Bearer {access_token}"}
+    user_response = requests.get(url, headers=headers)
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        print("It worked")
+        return user_data
+    else:
+        print(user_response.status_code)
+        return {"error" : "Failed to get user data"}
+    
